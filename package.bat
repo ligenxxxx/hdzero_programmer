f362b6050e729d88db70523815e7f776b0d36b54
ligenxxxx <59721724+ligenxxxx@users.noreply.github.com>
2024-04-09 18:52:51 +0800

    release resource folder from exe

--- package.bat
+++ package.bat
@@ -1,3 +1,1 @@
-pyinstaller -i HDZeroIcon.ico  -w -D hdzero_programmer.py
-xcopy /E /I /Y "resource" "dist/hdzero_programmer/resource"
-xcopy /E /I /Y "driver" "dist/hdzero_programmer/driver"
+pyinstaller -i HDZeroIcon.ico -w --onefile --add-data "resource;data_folder" hdzero_programmer.py
